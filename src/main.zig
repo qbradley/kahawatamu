a258412733be42a0391a75fb1b46326b5d5e1354
Quetzal Bradley <qbradley@qbradley.com>
2023-02-13 06:39:02 +0000

    Add sqlite support

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,82 @@
 const std = @import("std");
 const zap = @import("zap");
+const sqlite = @import("sqlite/sqlite.zig");
 
-fn on_request_verbose(r: zap.SimpleRequest) void {
+fn arrayListWrite(list: *std.ArrayList(u8), value: []const u8) std.mem.Allocator.Error!usize {
+    try list.appendSlice(value);
+    return value.len;
+}
+const StringWriter = std.io.Writer(*std.ArrayList(u8), std.mem.Allocator.Error, arrayListWrite);
+
+fn on_request_fallible(r: zap.SimpleRequest) !void {
     if (r.path) |the_path| {
         std.debug.print("PATH: {s}\n", .{the_path});
     }
 
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
     if (r.query) |the_query| {
         std.debug.print("QUERY: {s}\n", .{the_query});
     }
-    _ = r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>");
+
+    var database: sqlite.Database = try sqlite.open("/tmp/test.db");
+    defer database.close() catch unreachable;
+
+    var body = r.body orelse return error.NoBody;
+    var statement: sqlite.Statement = try database.prepare(body);
+    defer statement.finalize() catch unreachable;
+
+    const columns = statement.column_count();
+    std.debug.print("column count: {}\n", .{columns});
+
+    var buf = std.ArrayList(u8).init(gpa.allocator());
+    defer buf.deinit();
+    var stream = StringWriter{ .context = &buf };
+    var w = std.json.writeStream(stream, 10);
+
+    try w.beginObject();
+    try w.objectField("columns");
+    try w.beginArray();
+    var index: usize = 0;
+    while (index < columns) : (index += 1) {
+        try w.arrayElem();
+        try w.emitString(statement.column_name(index));
+    }
+    try w.endArray();
+    try w.objectField("rows");
+    try w.beginArray();
+
+    while (try statement.step()) |row| {
+        try w.arrayElem();
+        try w.beginArray();
+        var column_iter = row.get_columns();
+        while (column_iter.next(row)) |column| {
+            try w.arrayElem();
+            try w.emitString(column.get_text(row));
+        }
+        try w.endArray();
+    }
+
+    try w.endArray();
+    try w.endObject();
+
+    _ = r.sendBody(buf.items);
+}
+
+fn on_request_verbose(r: zap.SimpleRequest) void {
+    on_request_fallible(r) catch |e| {
+        switch (e) {
+            error.NoBody => {
+                r.setStatus(.bad_request);
+                _ = r.sendBody("No body was specified. The query should be specified as a body.");
+            },
+            else => {
+                r.setStatus(.internal_server_error);
+                _ = r.sendBody("Something went wrong");
+            },
+        }
+    };
 }
 
 fn on_request_minimal(r: zap.SimpleRequest) void {
@@ -17,6 +84,17 @@ fn on_request_minimal(r: zap.SimpleRequest) void {
 }
 
 pub fn main() !void {
+    var database: sqlite.Database = try sqlite.open("/tmp/test.db");
+    defer database.close() catch unreachable;
+
+    try database.exec_no_callback(
+        \\PRAGMA journal_mode=WAL;
+        \\CREATE TABLE IF NOT EXISTS properties (
+        \\  key TEXT,
+        \\  value TEXT
+        \\);
+    );
+
     var listener = zap.SimpleHttpListener.init(.{
         .port = 3000,
         .on_request = on_request_verbose,
@@ -29,8 +107,8 @@ pub fn main() !void {
 
     // start worker threads
     zap.start(.{
-        .threads = 2,
-        .workers = 2,
+        .threads = -1,
+        .workers = 1,
     });
 }
 
---
+++ src/sqlite/sqlite.zig
@@ -0,0 +1,120 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("sqlite3.h");
+});
+
+pub const Database = @This();
+pub const Error = error{
+    Failed,
+    NotImplemented,
+};
+
+db: *c.sqlite3,
+
+pub fn open(filename: [:0]const u8) Error!Database {
+    var db: ?*c.sqlite3 = undefined;
+    const rc: c_int = c.sqlite3_open(filename.ptr, &db);
+    if (rc != 0) {
+        return error.Failed;
+    }
+    return .{
+        .db = db orelse unreachable,
+    };
+}
+
+pub fn close(self: *Database) Error!void {
+    const rc = c.sqlite3_close(self.db);
+    if (rc != 0) {
+        return error.Failed;
+    }
+}
+
+pub fn exec_no_callback(self: *Database, script: [:0]const u8) Error!void {
+    const rc: c_int = c.sqlite3_exec(self.db, script.ptr, null, null, null);
+    if (rc != 0) {
+        return error.Failed;
+    }
+}
+
+pub const Column = struct {
+    index: usize,
+
+    pub fn get_text(column: Column, row: Row) [:0]const u8 {
+        const index = @intCast(c_int, column.index);
+        const text = c.sqlite3_column_text(row.stmt, index);
+        const len = @intCast(usize, c.sqlite3_column_bytes(row.stmt, index));
+        return text[0..len :0];
+    }
+};
+
+pub const ColumnIterator = struct {
+    index: usize,
+
+    pub fn next(iterator: *ColumnIterator, row: Row) ?Column {
+        const index = iterator.index;
+        if (index < row.column_count()) {
+            const result = Column{
+                .index = index,
+            };
+            iterator.index = index + 1;
+            return result;
+        } else {
+            return null;
+        }
+    }
+};
+
+pub const Row = struct {
+    stmt: *c.sqlite3_stmt,
+
+    pub fn get_columns(self: Row) ColumnIterator {
+        _ = self;
+        return .{
+            .index = 0,
+        };
+    }
+
+    pub fn column_count(self: Row) usize {
+        return @intCast(usize, c.sqlite3_column_count(self.stmt));
+    }
+};
+
+pub const Statement = struct {
+    stmt: *c.sqlite3_stmt,
+
+    pub fn step(self: *Statement) Error!?Row {
+        const rc: c_int = c.sqlite3_step(self.stmt);
+        return switch (rc) {
+            c.SQLITE_DONE => null,
+            c.SQLITE_ROW => .{ .stmt = self.stmt },
+            else => error.Failed,
+        };
+    }
+
+    pub fn column_name(self: Statement, index: usize) [:0]const u8 {
+        const text = c.sqlite3_column_name(self.stmt, @intCast(c_int, index));
+        return std.mem.sliceTo(text, 0);
+    }
+
+    pub fn column_count(self: Statement) usize {
+        return @intCast(usize, c.sqlite3_column_count(self.stmt));
+    }
+
+    pub fn finalize(self: *Statement) Error!void {
+        const rc: c_int = c.sqlite3_finalize(self.stmt);
+        if (rc != 0) {
+            return error.Failed;
+        }
+    }
+};
+
+pub fn prepare(self: *Database, sql: []const u8) Error!Statement {
+    var stmt: ?*c.sqlite3_stmt = undefined;
+    const rc: c_int = c.sqlite3_prepare_v2(self.db, sql.ptr, @intCast(c_int, sql.len), &stmt, null);
+    if (rc != 0) {
+        return error.Failed;
+    }
+    return .{
+        .stmt = stmt orelse unreachable,
+    };
+}
