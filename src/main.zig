afde006eadbebfe05381766dba6f7a9c6b9328c4
Quetzal Bradley <qbradley@qbradley.com>
2023-02-26 07:55:38 +0000

    beginning of networking

--- src/main.zig
+++ src/main.zig
@@ -8,18 +8,8 @@ const Proto1 = struct {
 
 pub export fn handle() void {
     std.debug.print("Child {}\n", .{lunatic.Process.process_id()});
-    switch (lunatic.Message.receive_all(1000)) {
-        .DataMessage => {
-            const p1 = receive(Proto1) catch unreachable;
-            std.debug.print("p1.x = {}\n", .{p1});
-        },
-        .SignalMessage => {
-            std.debug.print("Signal Message\n", .{});
-        },
-        .Timeout => {
-            std.debug.print("Timeout\n", .{});
-        },
-    }
+    const p1 = receive(Proto1) catch unreachable;
+    std.debug.print("p1.x = {}\n", .{p1});
 }
 
 pub fn send(process: lunatic.Process, comptime T: type, value: T) !void {
@@ -29,8 +19,20 @@ pub fn send(process: lunatic.Process, comptime T: type, value: T) !void {
 }
 
 pub fn receive(comptime T: type) !T {
-    const stream = lunatic.MessageReader{};
-    return try s2s.deserialize(stream.reader(), T);
+    while (true) {
+        switch (lunatic.Message.receive_all(0)) {
+            .DataMessage => {
+                const stream = lunatic.MessageReader{};
+                return try s2s.deserialize(stream.reader(), T);
+            },
+            .SignalMessage => {
+                std.debug.print("Signal Message\n", .{});
+            },
+            .Timeout => {
+                std.debug.print("Timeout\n", .{});
+            },
+        }
+    }
 }
 
 pub fn main() !void {
@@ -42,4 +44,32 @@ pub fn main() !void {
 
     const handle1 = try lunatic.Process.spawn("handle", .{}, .{});
     try send(handle1, Proto1, .{ .x = 5 });
+
+    const listener = try lunatic.Networking.Tcp.bind4(.{ 127, 0, 0, 1 }, 3001);
+    defer listener.deinit();
+
+    while (true) {
+        const socket = try listener.accept();
+        defer socket.deinit();
+        var buf: [1024]u8 = undefined;
+        var amount = try socket.read(buf[0..]);
+        std.debug.print("Received '{s}'\n", .{buf[0..amount]});
+        amount = try socket.write(buf[0..amount]);
+        std.debug.print("Sent {} bytes\n", .{amount});
+    }
+
+    //    const server = std.net.StreamServer.init(.{
+    //        .reuse_address = true,
+    //    });
+    //    defer server.deinit();
+    //
+    //    const address = std.net.Address.initIp4("127.0.0.1", 3000);
+    //    try server.listen(address);
+    //
+    //    while (true) {
+    //        var connection = try server.accept();
+    //        connection.stream.write("TEST\r\n");
+    //        connection.stream.close();
+    //    }
+    std.debug.print("Goodbye\n", .{});
 }
